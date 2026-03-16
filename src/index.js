be4ca946832ed755daa4c147cb4b8d2fba167ed0
Zoé Goujon <zoe.goujon@etu.univ-nantes.fr>
2026-03-16 19:17:47 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-// ./src/index.js
 module.exports = {
   register({ strapi }) {
     const userContentType = strapi.contentType('plugin::users-permissions.user');
-    userContentType.attributes = {
-      // Spread the existing native attributes to keep them
-      ...userContentType.attributes,
-      // Add your custom attributes
-      first_name:    { type: 'string' },
-      last_name:     { type: 'string' },
-      phone_number:  { type: 'string' },
-      helloasso_id:  { type: 'string' },
-      notif_mail:    { type: 'boolean', default: true },
-      notif_push:    { type: 'boolean', default: false }
-    };
+    
+    Object.assign(userContentType.attributes, {
+      first_name:   { type: 'string' },
+      last_name:    { type: 'string' },
+      phone_number: { type: 'string' },
+      helloasso_id: { type: 'string' },
+      notif_mail:   { type: 'boolean', default: true },
+      notif_push:   { type: 'boolean', default: false }
+    });
   },
   bootstrap({ strapi }) {},
 };
+
+///test
