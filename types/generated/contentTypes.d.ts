be21f2e68e6f649f776c5344dc759c549e50034c
= <zoegoujon85@gmail.com>
2026-04-04 21:53:26 +0200

    ?

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -702,6 +702,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<"oneToOne", "admin::user"> & Schema.Attribute.Private;
     ext: Schema.Attribute.String;
+    focalPoint: Schema.Attribute.JSON;
     folder: Schema.Attribute.Relation<"manyToOne", "plugin::upload.folder"> &
       Schema.Attribute.Private;
     folderPath: Schema.Attribute.String &
